1181745461752937a5a0ab79e76bd077aa9d463f
j-adamczyk <jakubadamczyk10@gmail.com>
2022-03-03 22:30:11 +0100

    Lab 1 template

---
+++ lab1_vertex_cover/vertex_cover/__init__.py
@@ -0,0 +1,1 @@
+from .brute_force import brute_force
---
+++ lab1_vertex_cover/vertex_cover/brute_force.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+from typing import Optional, Set
+
+from vertex_cover.types import VertexSets
+
+
+# TODO: add typing for graph
+def brute_force(graph, k: int) -> Optional[Set[int]]:
+    """
+    :param graph: graphs represented as GRAPH REPRESENTATION?
+    :param k: this many vertices have to cover the graphs
+    :return: set of vertices that create the cover if the solution exists,
+    otherwise None
+    """
+    # TODO: implement
+    return None
